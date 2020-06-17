d4342a60505a34207a01555238e24626d9b0b8d7
tugayapaydn <tugayapaydin999@gmail.com>
2020-06-17 23:28:15 +0300

    Update
    Plane class has implemented as designed.
    Added some feature methods for FlightSystem.

--- src/FlightSystem.java
+++ src/FlightSystem.java
@@ -2,8 +2,27 @@ import java.util.ArrayList;
 
 public class FlightSystem {
     ArrayList<Plane> avblPlanes;
+
+    //Requires implementation of PriorityQueue
     //Set<PriorityQueue<Flight>> flights;
     FlightSystem() {
         avblPlanes = new ArrayList<Plane>();
+
+        //Requires implementation of PriorityQueue
+        //flights = new TreeSet<PriorityQueue<Flight>>();
     }
+    
+    /* These methods will be completed after the implementation of the priority queue
+        public boolean addFlight() {
+
+        }
+
+        public boolean removeFlight() {
+
+        }
+
+        public void showFlights() {
+            
+        }
+    */
 }
--- src/Plane.java
+++ src/Plane.java
@@ -1,2 +1,24 @@
 public class Plane {
+    //Unique identification for planes
+    private static int PLANE_ID = 0;
+    
+    //Identification number of the plane
+    private String id;
+
+    //Capacity of the plane
+    private int capacity;
+
+    Plane(int capacity) {
+        this.id = String.valueOf(PLANE_ID++); 
+        this.capacity = capacity;
+    }
+    
+    //Getter methods
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public String getId() {
+        return id;
+    }
 }
