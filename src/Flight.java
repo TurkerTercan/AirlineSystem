03da753d02312ad5c240365242a9b2e4b7bc2587
TurkerTercan <turker.tercan2017@gtu.edu.tr>
2020-06-17 23:22:50 +0300

    Flight yazildi

--- src/Flight.java
+++ src/Flight.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+
 public class Flight {
+    //Data fields
+    private String ID;
+    private String plane;
+    private String destination;
+    private String setOff;
+    private String departTime;
+    private ArrayList<User> crew;
+    private ArrayList<Customer> customers;
+    private int seatCount;
+    private int remainingSeats;
+
+    //Constructors
+    public Flight( String ID, Plane plane, String destination, String setOff) {
+
+    }
+
 }
