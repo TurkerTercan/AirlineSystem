b3661d582243b9b438832396fd778c8190ddb5cc
TurkerTercan <turker.tercan2017@gtu.edu.tr>
2020-06-18 01:01:50 +0300

    Little changes on Flight Class

--- src/Flight.java
+++ src/Flight.java
@@ -1,9 +1,16 @@
 import java.util.ArrayList;
 
+/**
+ * This class represents a flight from this airline company.
+ * It has all necessary data fields and methods for a flight
+ */
 public class Flight {
     //Data fields
+    /** The ID of the flight */
     private String ID;
-    private String plane;
+    /** The Plane that is will fly */
+    private Plane plane;
+
     private String destination;
     private String setOff;
     private String departTime;
@@ -14,7 +21,10 @@ public class Flight {
 
     //Constructors
     public Flight( String ID, Plane plane, String destination, String setOff) {
-
+        this.ID = ID;
+        this.plane = plane;
+        this.destination = destination;
+        this.setOff = setOff;
     }
 
 }
