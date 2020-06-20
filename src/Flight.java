78cf7255b685fb81b1d7c0b5cb27c68b6cb297d9
Wustenblume <emirli@gtu.edu.tr>
2020-06-20 20:47:36 +0300

    PriorityQueue added and additions were  made in Flight class

--- src/Flight.java
+++ src/Flight.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
  * This class represents a flight from this airline company.
  * It has all necessary data fields and methods for a flight
  */
-public class Flight {
+public class Flight<E> implements Comparable< E > {
     //Data fields
     /** The ID of the flight */
     private String ID;
@@ -15,6 +15,11 @@ public class Flight {
     private String destination;
     /** The plane's set off zone */
     private String setOff;
+
+    public String getDepartTime() {
+        return departTime;
+    }
+
     /** The time that the plane will be departed */
     private String departTime;
     /** List of crew */
@@ -63,5 +68,64 @@ public class Flight {
                 "Set Off: " + setOff + "\tDestination: " + destination + "\n" +
                 "Depart Time: " + departTime + "\n" +
                 customers.size() + " Customers, " + crew.size() + " Crew members\n";
+    }
+
+    @Override
+    public int compareTo(E o) {
+        //Split departTime
+        String[] time1 = departTime.split("/");
+        int day =  Integer.parseInt(time1[0]);
+        int  month = Integer.parseInt(time1[1]);
+
+        String[] time2 = time1[2].split(",");
+
+        int year = Integer.parseInt(time2[0]);
+        String[] time3 = time2[1].split(":");
+        int hour = Integer.parseInt(time3[0]);
+        int minute = Integer.parseInt(time3[1]);
+
+        //Split parameter
+        time1 = o.toString().split("/");
+        int oday = Integer.parseInt(time1[0]);
+        int omonth = Integer.parseInt(time1[1]);
+
+        time2 = time1[2].split(",");
+
+        int oyear = Integer.parseInt(time2[0]);
+        time3 = time2[1].split(":");
+        int ohour = Integer.parseInt(time3[0]);
+        int ominute = Integer.parseInt(time3[1]);
+
+        //Compare year
+        if(year < oyear) {
+            return -1;
+        } else if(year > oyear)
+            return 1;
+
+        //compare month
+        if(month < omonth)
+            return -1;
+        else if(month > omonth)
+            return 1;
+
+        //compare day
+        if(day < oday)
+            return -1;
+        else if(day > oday)
+            return 1;
+
+        //compare hour
+        if(hour < ohour)
+            return -1;
+        else if(hour > ohour)
+            return 1;
+
+        //compare minute
+        if(minute < ominute)
+            return -1;
+        else if(minute > ominute)
+            return 1;
+
+        return 0;
     }
 }
