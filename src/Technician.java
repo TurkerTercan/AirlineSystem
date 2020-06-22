14ac67cadd85d80fe9f190e0206501bc87db247d
Esra Eryılmaz <45772354+esraeryilmaz@users.noreply.github.com>
2020-06-22 22:15:45 +0300

    Update Technician.java
    Technician Uptaded

--- src/Technician.java
+++ src/Technician.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Queue;
 
 /**
  * Represents technicians of the airline system.
@@ -6,6 +7,7 @@ import java.util.Scanner;
 public class Technician extends User {
 	private Scanner input;
     private boolean LogedIn = false;
+    private Queue<Plane> planeMaintance;
 
     public Technician(String id, String password) {
         super(id, password);
@@ -29,6 +31,47 @@ public class Technician extends User {
 
     @Override
     public void menu() {
+        int choice = -1;
+        while (choice!=0) {
+            System.out.println("\nMain menu:");
+            System.out.println("Please choose an action:");
+            System.out.println("0-Up\n1-Maintenance Confirm\n2-Show Maintenance Airplane Queue");
+            System.out.print("\nChoice:");
+            choice = input.nextInt();
+            switch (choice){
+                case 1:
+                    maintenanceConfirm();
+                    break;
 
+                case 2:
+                    getPlaneMaintance();
+                    break;
+            }
+        }
+    }
+
+    private void maintenanceConfirm()
+    {
+        if(planeMaintance.size() != 0) {
+            Plane maintain = planeMaintance.poll();
+            System.out.print("Plane " + maintain.getId() +" is maintained!");
+        }
+        else {
+            System.out.print("Plane maintain queue is empty!");
+        }
+
+    }
+
+    private void showMaintenanceQueue() {
+        System.out.print("There are planes waiting to be maintained : ");
+        System.out.print(getPlaneMaintance().toString());
+    }
+
+    public Queue<Plane> getPlaneMaintance() {
+        return planeMaintance;
+    }
+
+    public void setPlaneMaintance(Queue<Plane> planeMaintance) {
+        this.planeMaintance = planeMaintance;
     }
 }
