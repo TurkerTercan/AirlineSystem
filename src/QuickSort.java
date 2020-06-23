398c45cad6b86fa176a4217869e4e2710277e4f4
Wustenblume <emirli@gtu.edu.tr>
2020-06-23 17:47:04 +0300

    quicksort javadoc

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -16,6 +16,13 @@ public class QuickSort {
         // Sort the whole table.
         quickSort(table, 0, table.size() - 1);
     }
+
+    /** Sort a part of the table using the quicksort algorithm.
+     post: The part of table from first through last is sorted.
+     @param table The list to be sorted
+     @param first The index of the low bound
+     @param last The index of the high bound
+     */
     public static void quickSort(List<Flight> table, int first, int last) {
         if (first < last) { // There is data to be sorted.
             // Partition the table.
@@ -26,6 +33,15 @@ public class QuickSort {
             quickSort(table, pivIndex + 1, last);
         }
     }
+
+    /** Partition the table so that values from first to pivIndex
+     are less than or equal to the pivot value, and values from
+     pivIndex to last are greater than the pivot value.
+     @param table The table to be partitioned
+     @param first The index of the low bound
+     @param last  The index of the high bound
+     @return The location of the pivot value
+     */
     private static int partition(List<Flight> table, int first, int last) {
         // Select the first item as the pivot value.
         double pivot =  table.get(first).getPricePerSeat();
