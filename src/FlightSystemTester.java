6d121cf4fae0d11de2759f04f8e3b60fad721383
tugayapaydn <tugayapaydin999@gmail.com>
2020-06-23 22:10:36 +0300

    Update FlightSystemTester
    Implemented test_removeFlights method

--- src/FlightSystemTester.java
+++ src/FlightSystemTester.java
@@ -1,6 +1,7 @@
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Map;
 import java.util.Random;
 
 import sun.net.www.content.text.plain;
@@ -21,26 +22,54 @@ public class FlightSystemTester {
         flight.addPlane(new Plane(101));
 
         flight.ShowAllPlanes();
-        test_addFlight(LOWER_TEST_COUNT);
+        //test_addFlight(LOWER_TEST_COUNT);
+        test_removeFlight(LOWER_TEST_COUNT);
     }
 
     private static FlightSystem test_addFlight(int test_count) throws FileNotFoundException {
-        System.out.println("Testing addFlight for "+test_count+" times");
+        System.out.println("Adding "+test_count+" flights to the system.");
         System.out.println("Expected Output: False if there is already a flight with the same properties, otherwise; true.");
-        Random rand = new Random();
+        
         FlightSystem system = new FlightSystem();
         ArrayList<String> cities = system.getCity();
         int city_size = cities.size();
         int f_id = 1;
         int p_id = 1;
+
+        Random rand = new Random();
         for (int i = 0; i < test_count; i++) {
             String c1 = cities.get(rand.nextInt(city_size));
             String c2 = cities.get(rand.nextInt(city_size));
 
-            System.out.print("\tAdding a new flight: "+c1+" - "+c2+": ");
-            System.out.println(system.addFlight(new Flight(String.valueOf(f_id++), new Plane(String.valueOf(p_id++), 50), c1, c2, time_test, 500)));
+            System.out.print("\tAdding a new flight: "+c1+" -> "+c2+": ");
+            System.out.println(system.addFlight(new Flight(String.valueOf(f_id++), new Plane(String.valueOf(p_id++), 50), c2, c1, time_test, 500)));
         }
+        return system;
+    }
 
+    private static FlightSystem test_removeFlight(int test_count) throws FileNotFoundException {
+        System.out.println("Removing all flights of a system that has "+test_count+" flights recorded.");
+        FlightSystem system = test_addFlight(test_count);
+        ArrayList<String> cities = system.getCity();
+        int city_size = cities.size();
+        Map<String,Map<String, PriorityQueue<Flight>>> flights = system.getFlight_map();
+        int p_id = 1;
+        for (int i = 0; i < city_size; i++) {
+            Map<String, PriorityQueue<Flight>> f = flights.get(cities.get(i));
+            if (f != null) {
+                for (int j = 0; j < city_size; j++) {
+                    PriorityQueue<Flight> f2 = f.get(cities.get(j));
+                    if (f2 != null) {
+                        for (int k = 0; k < f2.size(); k++) {
+                            Flight f3 = f2.peek();
+                            System.out.print("\tRemoving the flight: "+f3.getSetOff()+" -> "+f3.getDestination()+": ");
+                            
+                            System.out.println(system.removeFlight(new Flight(f3.getID(), new Plane(String.valueOf(p_id++), 50), f3.getDestination(), f3.getSetOff(), time_test, 500)));
+                        }
+                    }
+                }
+            }
+        }
         return system;
     }
 }
